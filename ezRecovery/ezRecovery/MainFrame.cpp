3d35facc4afd9407b71c2b03ee679cd4ceec1d09
NotCaludio <101145630+NotCaludio@users.noreply.github.com>
2024-05-26 23:29:03 -0600

    This is the final version everything works v2

--- ezRecovery/ezRecovery/MainFrame.cpp
+++ ezRecovery/ezRecovery/MainFrame.cpp
@@ -4,10 +4,7 @@
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 
-
 	panel = new wxPanel(this);
-	
-	
 	wxFont headlineFont(wxFontInfo(wxSize(0, 20)).Bold());
 	wxStaticText* headlineText = new wxStaticText(panel, wxID_ANY, "Recuperación de archivos", wxPoint(0, 22), wxSize(500, -1), wxALIGN_CENTRE_HORIZONTAL);
 	headlineText->SetFont(headlineFont);
@@ -35,43 +32,8 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 
 	statusMessage = new wxStaticText(panel, wxID_ANY, path, wxPoint(0, 100), wxSize(500, -1), wxALIGN_CENTRE_HORIZONTAL);
 	statusMessage->SetBackgroundColour(*wxBLUE);
-
-
-
-	/*button->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
-	slider->Bind(wxEVT_SLIDER, &MainFrame::OnSliderChanged, this);
-	textCtrl->Bind(wxEVT_TEXT, &MainFrame::OnTextChanged, this);
-
-	button->Unbind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
-	+*/
 	CreateStatusBar();
-	/*
-	wxButton* button = new wxButton(panel, wxID_ANY, "Recuperar", wxPoint(150, 50), wxSize  (100,35));
 
-	wxCheckBox* checkBox = new wxCheckBox(panel, wxID_ANY, "Guardar firmas de los archivos", wxPoint(550,55), wxDefaultSize, wxCHK_3STATE | wxCHK_ALLOW_3RD_STATE_FOR_USER);
-
-	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, "Recuperacino de archivos", wxPoint(120, 150), wxSize(400,-1), wxALIGN_CENTRE_HORIZONTAL);
-	staticText->SetBackgroundColour(*wxLIGHT_GREY);
-
-	wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, "TextCtrl - editable", wxPoint(500, 145), wxSize(200, -1));
-
-	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1), wxSL_VALUE_LABEL);
-	
-	wxGauge* gauge = new wxGauge(panel, wxID_ANY, 100, wxPoint(500, 255), wxSize(200, -1));
-	gauge->SetValue(50);
-
-	wxArrayString choices;
-	choices.Add("Item A");
-	choices.Add("Item B");
-	choices.Add("Item C");
-
-	wxChoice* choice = new wxChoice(panel, wxID_ANY, wxPoint(150, 375), wxSize(100, -1), choices,wxCB_SORT);
-	choice->Select(0);
-
-	wxSpinCtrl* spinCtrl = new wxSpinCtrl(panel, wxID_ANY, " ", wxPoint(550, 375), wxSize(100, -1), wxSP_WRAP);
-
-	wxListBox* listBox = new wxListBox(panel, wxID_ANY, wxPoint(150, 475), wxSize(100, -1), choices );
-	wxRadioBox* radioBox = new wxRadioBox(panel, wxID_ANY ,"radio", wxPoint(485, 475), wxDefaultSize, choices,3 , wxRA_SPECIFY_ROWS);*/
 }
 
 void MainFrame::OnButtonClicked(wxCommandEvent& evt) {
